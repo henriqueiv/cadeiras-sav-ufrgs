8d3dbd6c78a651dd94e625c9d514258174097268
Henrique Indalencio Valcanaia <henrique.valcanaia@hotmail.com>
2015-02-05 01:43:03 -0200

    Commit
    Commit

---
+++ cadeiras-sav-ufrgs.js
@@ -0,0 +1,7 @@
+var texto = "SUPOSTAMENTE você cursará: \n\n";
+$('[id^="tabbed_nome_atividade"').find("h2").each(function(i, obj) {
+	temp = obj.innerText;
+	temp = temp.replace(/(\r\n|\r|\n)+/g, '$1');
+	texto += (i+1) + " - " + temp.replace(/\s+/g, ' ') + ";\n";
+});
+alert(texto);
