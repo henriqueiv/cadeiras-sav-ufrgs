c84653234ee6cb430653cd7b1b93c9ad3804b28c
Henrique Indalencio Valcanaia <henrique.valcanaia@hotmail.com>
2015-07-21 14:09:23 -0300

    Alteração
    Melhorado o super código.

--- cadeiras-sav-ufrgs.js
+++ cadeiras-sav-ufrgs.js
@@ -2,6 +2,6 @@ var texto = "SUPOSTAMENTE você cursará: \n\n";
 $('[id^="tabbed_nome_atividade"').find("h2").each(function(i, obj) {
 	temp = obj.innerText;
 	temp = temp.replace(/(\r\n|\r|\n)+/g, '$1');
-	texto += (i+1) + " - " + temp.replace(/\s+/g, ' ') + ";\n";
+	texto += (++i) + " - " + temp.replace(/\s+/g, ' ') + ";\n";
 });
 alert(texto);
